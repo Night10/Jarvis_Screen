8783e20c9decd1d2a7fe1ebec69f20dfd0063b63
headstrongsolutions <chris@headstrong.solutions>
2017-10-12 12:58:33 +0100

    refactored pir to a class

--- testPIR.py
+++ testPIR.py
@@ -1,13 +1,18 @@
 import RPi.GPIO as GPIO
-import time
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.IN)         #Read output from PIR motion sensor
-while True:
-       i=GPIO.input(13)
-       if i==0:                 #When output from motion sensor is LOW
-             print ("No intruders",i)
-             time.sleep(0.1)
-       elif i==1:               #When output from motion sensor is HIGH
-             print ("Intruder detected",i)
-             time.sleep(0.1)
+
+class pir_sensor:
+    # describes the type
+    typeDescription = 'PIR Sensor'
+
+    def __init__(self):
+        self.sensor_input_pin = 13
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.sensor_input_pin, GPIO.IN) # Read output from PIR motion sensor
+
+    def get_reading(self):
+        i=GPIO.input(13)
+        if i==0:          # When output from motion sensor is LOW
+            return False
+        elif i==1:        # When output from motion sensor is HIGH
+            return True
