7dd3df51afa3c88792382d4517fa4aeaa9598b49
headstrongsolutions <chris@headstrong.solutions>
2017-10-14 16:37:48 +0100

    created a working dht package class, removed redundant testdht script

--- dht_package.py
+++ dht_package.py
@@ -1,4 +1,5 @@
-import Adafruit_DHT, getip, gettime
+import Adafruit_DHT
+import requests
 
 class dht_package:
     typeDescription = 'Sends a DHT Package to Jarvis Brain'
@@ -6,7 +7,7 @@ class dht_package:
     def __init__(self):
         self.jarvis_brain_endpoint = "http://jarvis_brain:5000/InternalComms"
         self.jarvis_brain_heartbeat = "/heartbeat"
-        self.jarvis_brain_post_dht = "/CollectDHT"
+        self.jarvis_brain_post_dht = "/CollectDHT/"
         self.sensor = Adafruit_DHT.DHT11
         self.sensor_pin = 6
         self.dht_package_shape = {}
@@ -15,19 +16,37 @@ class dht_package:
         self.dht_package_shape["voltage"] = -1
         self.dht_package_shape["location"] = "House"
         self.dht_package_shape["message"] = "Initialised"
-        self.dht_package_shape["IP"] = "0.0.0.0"
+        self.dht_package_post_response = ""
 
     def get_reading(self):
-        humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.sensor_pin)
-        self.dht_package_shape["IP"] = getip().get_ip_address()
-        self.dht_package_shape["temp"] = temperature
-        self.dht_package_shape["humi"] = humidity
-        self.dht_package_shape["message"] = "Collected at " + gettime().get_time_now()
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.sensor_pin)
+            self.dht_package_shape["temp"] = temperature
+            self.dht_package_shape["humi"] = humidity
+            self.dht_package_shape["message"] = ""
+            return True
+        except:
+            print("errored collecting temperature")
+            return False
 
     def ship_reading(self):
-        
-        return true
-
-# dht_package = dht_package()
-# print(dht_package.get_reading())
-
+        if self.get_reading():
+            postData = '{'
+            postData += '"Temperature":"'+str(self.dht_package_shape["temp"])+'",'
+            postData += '"Humidity":"'+str(self.dht_package_shape["humi"])+'",'
+            postData += '"BatteryLevel":"'+str(self.dht_package_shape["voltage"])+'",'
+            postData += '"Location":"'+self.dht_package_shape["location"]+'",'
+            postData += '"InternalComms.Name":"DHT Temperature Package",'
+            postData += '"InternalComms.Received":"",'
+            postData += '"InternalComms.Sent":"",'
+            postData += '"InternalComms.State":"Sent",'
+            postData += '"InternalComms.Message":"'+self.dht_package_shape["message"]+'"}'
+            url = self.jarvis_brain_endpoint + self.jarvis_brain_post_dht
+            headers = {'content-type': 'application/json'}
+            try:
+                resp = requests.post(url, data=postData, headers=headers)
+                self.dht_package_post_response = resp.reason
+                return True
+            except (requests.ConnectionError):
+                print("something went wrong sending the data to Jarvis Brain")
+                return False
