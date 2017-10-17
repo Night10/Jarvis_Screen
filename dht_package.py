52c30ce25a0541e9a70d62872ac3c944c2e97ac8
headstrongsolutions <chris@headstrong.solutions>
2017-10-17 21:03:07 +0100

    further parametised url, added reading collection, started get garden temperature

--- dht_package.py
+++ dht_package.py
@@ -5,18 +5,20 @@ class dht_package:
     typeDescription = 'Sends a DHT Package to Jarvis Brain'
 
     def __init__(self):
-        self.jarvis_brain_endpoint = "http://jarvis_brain:5000/InternalComms"
+        self.jarvis_brain_endpoint = "http://jarvis_brain:5000/"
         self.jarvis_brain_heartbeat = "/heartbeat"
-        self.jarvis_brain_post_dht = "/CollectDHT/"
+        self.jarvis_brain_post_dht = "InternalComms/CollectDHT/"
+        self.jarvis_brain_collect_dht = "ExternalComms/getLast7DaysTemperature/"
         self.sensor = Adafruit_DHT.DHT11
         self.sensor_pin = 6
         self.dht_package_shape = {}
         self.dht_package_shape["temp"] = -1
         self.dht_package_shape["humi"] = -1
         self.dht_package_shape["voltage"] = -1
-        self.dht_package_shape["location"] = "House"
+        self.dht_package_shape["location"] = "House/"
         self.dht_package_shape["message"] = "Initialised"
         self.dht_package_post_response = ""
+        self.dht_collection = []
 
     def get_reading(self):
         try:
@@ -50,3 +52,20 @@ class dht_package:
             except (requests.ConnectionError):
                 print("something went wrong sending the data to Jarvis Brain")
                 return False
+
+
+    def collect_readings(self, location):
+        if location is not None:
+            raw_collection = requests.get(self.jarvis_brain_endpoint + self.jarvis_brain_collect_dht + location)
+            self.dht_collection = raw_collection.json()
+
+    def get_latest_temperature(self, location):
+        if location is not None:
+            self.collect_readings(location)
+            readings = self.dht_collection
+            return readings[0]
+
+    def get_lowest_garden_temperature(self):
+        readings = self.get_latest_temperature("Garden/")
+        # TODO - convert the set into a date ordered collection
+        #        and cut the lowest in the last 24 hours
